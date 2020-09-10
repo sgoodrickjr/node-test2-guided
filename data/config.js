7e862ea4e17ed4c23e8f05a6e6b3cb83c684fd61
Jason Maurer <jason@maur.co>
2020-09-09 19:38:33 -0600

    Code from guided lecture

--- data/config.js
+++ data/config.js
@@ -1,4 +1,6 @@
 const knex = require("knex")
 const knexfile = require("../knexfile")
 
-module.exports = knex(knexfile)
+const env = process.env.NODE_ENV || "development"
+
+module.exports = knex(knexfile[env])
